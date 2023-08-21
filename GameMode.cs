97a9526a61b06a720e62f8ed05857f728be7def9
S-Katagiri <katagiri.stsh@gmail.com>
2023-08-21 22:29:26 +0900

    ModOption and Cr eatureData

--- GameMode.cs
+++ GameMode.cs
@@ -1,91 +1,82 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using ThunderRoad;
+﻿using ThunderRoad;
 using UnityEngine;
 using Extensions;
 
 namespace GettingOverIt
 {
-    public class GameMode : LevelModule
+    public class GettingIt : ThunderScript
     {
-        public bool turnAllowed = true;
-        public bool highJump = true;
-        public bool fastSwing = true;
-        private const float gravityMult = 0.7f;
-        private const float massMult = 0.1f;
-        private readonly Vector2 springDamper = new Vector2(4000f, 100f);
-        private readonly Vector2 springDamper2HMult = new Vector2(2f, 1f);
-        private const float forceMaxPosition = 10000f;
-        private const float forceMaxRotation = 10000f;
-        private Vector2 _forcePositionSpringDamper;
-        private Vector2 _forceRotationSpringDamper;
-        private Vector2 _forceRotationSpringDamper2HMult;
-        private Vector2 _forcePositionSpringDamper2HMult;
-        private float _forceMaxPosition;
-        private float _forceMaxRotation;
-        public override IEnumerator OnLoadCoroutine()
+        [ModOptionCategory("General", 0, "ModOpts.category_general")]
+        [ModOption("Getting Over It", nameLocalizationId = "ModOpts.getting_over_it", order = 0, defaultValueIndex = 1)]
+        [ModOptionTooltip("A", "ModOpts.getting_over_it_desc")]
+        public static bool OptEnabled { get; set; }
+        [ModOptionCategory("General", 0, "ModOpts.category_general")]
+        [ModOption("Enable Turn", nameLocalizationId = "ModOpts.enable_turn", order = 1, defaultValueIndex = 0)]
+        [ModOptionTooltip("A", "ModOpts.enable_turn_desc")] 
+        public static bool OptEnableTurn
         {
-            this.turnAllowed = this.level.ParseLevelOption("turnAllowed", true);
-            this.highJump = this.level.ParseLevelOption("highJumpt", true);
-            this.fastSwing = this.level.ParseLevelOption("fastSwing", true);
-            EventManager.OnPlayerSpawned += this.ModifyPlayer_onSpawn;
+            get => _optEnableTurn;
+            set
+            {
+                _optEnableTurn = value;
+                if(Player.local != null)
+                {
+                    Player.TogglePlayerTurn(_optEnableTurn);
+                }
+            }
+        }
+        private static bool _optEnableTurn = false;
+        [ModOptionCategory("General", 0, "ModOpts.category_general")]
+        [ModOption("High Jump", nameLocalizationId = "ModOpts.high_jump", order = 2, defaultValueIndex = 1)]
+        [ModOptionTooltip("A", "ModOpts.high_jump_desc")]
+        public static bool OptHighJump { get; set; }
+        [ModOptionCategory("General", 0, "ModOpts.category_general")]
+        [ModOption("Fast Swing", nameLocalizationId = "ModOpts.fast_swing", order = 3, defaultValueIndex = 1)]
+        [ModOptionTooltip("A", "ModOpts.fast_swing_desc")]
+        public static bool OptFastSwing { get; set; }
+        public override void ScriptLoaded(ModManager.ModData modData)
+        {
+            base.ScriptLoaded(modData);
+        }
+        public override void ScriptEnable()
+        {
+            Player.onSpawn += this.ModifyPlayer_onSpawn;
             EventManager.onCreatureSpawn += this.EventManager_onCreatureSpawn;
             EventManager.onLevelUnload += EventManager_onLevelUnload;
-            Debug.Log("[GettingOverIt] Getting Over It mode activated");
-            return base.OnLoadCoroutine();
+            Debug.Log("Getting Over It mode activated");
+            base.ScriptEnable();
         }
-        public override void OnUnload()
+        public override void ScriptDisable()
         {
-            EventManager.OnPlayerSpawned -= this.ModifyPlayer_onSpawn;
+            Player.onSpawn -= this.ModifyPlayer_onSpawn;
             EventManager.onCreatureSpawn -= this.EventManager_onCreatureSpawn;
             EventManager.onLevelUnload -= EventManager_onLevelUnload;
-            Debug.Log("[GettingOverIt] Getting Over It mode deactivated");
-            base.OnUnload();
+            Debug.Log("Getting Over It mode deactivated");
+            base.ScriptDisable();
         }
-        private void ModifyPlayer_onSpawn()
+        private void ModifyPlayer_onSpawn(Player player)
         {
-            if(this.turnAllowed)
+            if (OptEnabled)
             {
-                Player.TogglePlayerMovement(false);
-                Player.TogglePlayerJump(false);
+                Player.crouchOnJump = false; // ther pot interrupt view if enabled
+                CreatureData cd = Catalog.GetData<CreatureData>(Player.characterData.creatureId);
+                this.BackupfastSwing(cd);
             }
-            else
-            {
-                Player.TogglePlayerGroundMovement(false);
-            }
-            Player.crouchOnJump = false; // ther pot interrupt view if enabled
-            CreatureData cd = Catalog.GetData<CreatureData>(Player.characterData.creatureId);
-            this._forceMaxRotation = cd.forceMaxRotation;
-            this._forceMaxPosition = cd.forceMaxPosition;
-            this._forcePositionSpringDamper = cd.forcePositionSpringDamper;
-            this._forceRotationSpringDamper = cd.forceRotationSpringDamper;
-            this._forcePositionSpringDamper2HMult= cd.forcePositionSpringDamper2HMult;
-            this._forceRotationSpringDamper2HMult = cd.forceRotationSpringDamper2HMult;
         }
         private void EventManager_onCreatureSpawn(Creature creature)
         {
-            Debug.Log("[GettingOverIt] Player creature spawned As Diogenes");
-            if(creature.isPlayer)
+            if(creature.isPlayer && OptEnabled)
             {
+                TogglePlayerMove(false);
                 // TODO: keep crouching
                 creature.currentLocomotion.SetPhysicModifier(
                     this,
-                    gravityMultiplier: this.highJump ? (float?)gravityMult : null,
-                    massMultiplier: this.highJump ? massMult: -1);
-                if(this.fastSwing)
+                    gravityMultiplier: OptHighJump ? (float?)gravityMult : null,
+                    massMultiplier: OptHighJump ? massMult: -1);
+                if(OptFastSwing)
                 {
-                    //TODO: refactor
-                    creature.data.forceMaxPosition = forceMaxPosition;
-                    creature.data.forceMaxRotation = forceMaxRotation;
-                    creature.data.forcePositionSpringDamper = this.springDamper;
-                    creature.data.forceRotationSpringDamper = this.springDamper;
-                    creature.data.forcePositionSpringDamper2HMult = this.springDamper2HMult;
-                    creature.data.forceRotationSpringDamper2HMult = this.springDamper2HMult;                    
+                    CreatureData cd = Catalog.GetData<CreatureData>("GettingOverItCreatureData");
+                    this.ApplyCreatureDataValues(creature, cd);
                 }
                 creature.equipment.UnequipAllWardrobes(true);
                 Catalog.InstantiateAsync(
@@ -109,15 +100,41 @@ namespace GettingOverIt
         }
         private void EventManager_onLevelUnload(LevelData levelData, EventTime eventTime)
         {
-            if (eventTime == EventTime.OnStart)
+            if (eventTime == EventTime.OnStart && OptEnabled)
             {
-                Player.currentCreature.data.forceMaxPosition = this._forceMaxPosition;
-                Player.currentCreature.data.forceMaxRotation = this._forceMaxRotation;
-                Player.currentCreature.data.forcePositionSpringDamper = this._forcePositionSpringDamper;
-                Player.currentCreature.data.forceRotationSpringDamper = this._forceRotationSpringDamper;
-                Player.currentCreature.data.forcePositionSpringDamper2HMult = this._forcePositionSpringDamper2HMult;
-                Player.currentCreature.data.forceRotationSpringDamper2HMult = this._forceRotationSpringDamper2HMult;
+                this.ApplyCreatureDataValues(Player.currentCreature, this.creatureData_backup) ;
             }
         }
+        private void BackupfastSwing(CreatureData cd)
+        {
+            this.creatureData_backup.forceMaxPosition = cd.forceMaxPosition;
+            this.creatureData_backup.forceMaxRotation = cd.forceMaxRotation;
+            this.creatureData_backup.forcePositionSpringDamper = cd.forcePositionSpringDamper;
+            this.creatureData_backup.forceRotationSpringDamper = cd.forceRotationSpringDamper;
+            this.creatureData_backup.forcePositionSpringDamper2HMult = cd.forcePositionSpringDamper2HMult;
+            this.creatureData_backup.forceRotationSpringDamper2HMult = cd.forceRotationSpringDamper2HMult;
+        }
+        private static void TogglePlayerMove(bool value)
+        {
+            if (Player.local != null)
+            {
+                Player.TogglePlayerMovement(value);
+                Player.TogglePlayerJump(value);
+                Player.TogglePlayerGroundMovement(value);
+                Player.TogglePlayerTurn(_optEnableTurn);
+            }
+        }
+        private void ApplyCreatureDataValues(Creature creature, CreatureData creatureData)
+        {
+            creature.data.forceMaxPosition = this.creatureData_backup.forceMaxPosition;
+            creature.data.forceMaxRotation = this.creatureData_backup.forceMaxRotation;
+            creature.data.forcePositionSpringDamper = this.creatureData_backup.forcePositionSpringDamper;
+            creature.data.forceRotationSpringDamper = this.creatureData_backup.forceRotationSpringDamper;
+            creature.data.forcePositionSpringDamper2HMult = this.creatureData_backup.forcePositionSpringDamper2HMult;
+            creature.data.forceRotationSpringDamper2HMult = this.creatureData_backup.forceRotationSpringDamper2HMult;
+        }
+        private CreatureData creatureData_backup = new();
+        private const float gravityMult = 0.7f;
+        private const float massMult = 0.1f;
     }
 }
