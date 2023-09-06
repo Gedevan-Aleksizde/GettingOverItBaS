afaa412f8435e85dbacf172c8e8ac55decf84e51
S-Katagiri <katagiri.stsh@gmail.com>
2023-09-07 00:24:38 +0900

    avoid to apply in MainMenu and Home level

--- GameMode.cs
+++ GameMode.cs
@@ -1,17 +1,21 @@
 ﻿using ThunderRoad;
 using UnityEngine;
 using Extensions;
+using System.Security.Policy;
 
 namespace GettingOverIt
 {
     public class GettingIt : ThunderScript
     {
         [ModOptionCategory("General", 0, "ModOpts.category_general")]
-        [ModOption("Getting Over It", nameLocalizationId = "ModOpts.getting_over_it", order = 0, defaultValueIndex = 1)]
+        [ModOption("getting_over_it", nameLocalizationId = "ModOpts.getting_over_it", order = 0, defaultValueIndex = 1)]
         [ModOptionTooltip("A", "ModOpts.getting_over_it_desc")]
         public static bool OptEnabled { get; set; }
         [ModOptionCategory("General", 0, "ModOpts.category_general")]
-        [ModOption("Enable Turn", nameLocalizationId = "ModOpts.enable_turn", order = 1, defaultValueIndex = 0)]
+        [ModOption("enable_in_home", nameLocalizationId = "ModOpts.enable_in_home", order = 1, defaultValueIndex = 0)]
+        public static bool OptEnabledHome { get; set; }
+        [ModOptionCategory("General", 0, "ModOpts.category_general")]
+        [ModOption("enable_turn", nameLocalizationId = "ModOpts.enable_turn", order = 2, defaultValueIndex = 0)]
         [ModOptionTooltip("A", "ModOpts.enable_turn_desc")] 
         public static bool OptEnableTurn
         {
@@ -27,11 +31,11 @@ namespace GettingOverIt
         }
         private static bool _optEnableTurn = false;
         [ModOptionCategory("General", 0, "ModOpts.category_general")]
-        [ModOption("High Jump", nameLocalizationId = "ModOpts.high_jump", order = 2, defaultValueIndex = 1)]
+        [ModOption("high_jump", nameLocalizationId = "ModOpts.high_jump", order = 3, defaultValueIndex = 1)]
         [ModOptionTooltip("A", "ModOpts.high_jump_desc")]
         public static bool OptHighJump { get; set; }
         [ModOptionCategory("General", 0, "ModOpts.category_general")]
-        [ModOption("Fast Swing", nameLocalizationId = "ModOpts.fast_swing", order = 3, defaultValueIndex = 1)]
+        [ModOption("fast_swing", nameLocalizationId = "ModOpts.fast_swing", order = 4, defaultValueIndex = 1)]
         [ModOptionTooltip("A", "ModOpts.fast_swing_desc")]
         public static bool OptFastSwing { get; set; }
         public override void ScriptLoaded(ModManager.ModData modData)
@@ -40,33 +44,36 @@ namespace GettingOverIt
         }
         public override void ScriptEnable()
         {
+            Debug.Log("Activating Getting Over It mode...");
+            this.crouchOnJump = Player.crouchOnJump;
             Player.onSpawn += this.ModifyPlayer_onSpawn;
             EventManager.onCreatureSpawn += this.EventManager_onCreatureSpawn;
             EventManager.onLevelUnload += EventManager_onLevelUnload;
-            Debug.Log("Getting Over It mode activated");
             base.ScriptEnable();
         }
         public override void ScriptDisable()
         {
+            Debug.Log("Deactivating Getting Over It mode...");
+            Player.crouchOnJump = this.crouchOnJump;
             Player.onSpawn -= this.ModifyPlayer_onSpawn;
             EventManager.onCreatureSpawn -= this.EventManager_onCreatureSpawn;
             EventManager.onLevelUnload -= EventManager_onLevelUnload;
-            Debug.Log("Getting Over It mode deactivated");
+            TogglePlayerMove(true);
             base.ScriptDisable();
         }
         private void ModifyPlayer_onSpawn(Player player)
         {
             if (OptEnabled)
             {
-                Player.crouchOnJump = false; // ther pot interrupt view if enabled
                 CreatureData cd = Catalog.GetData<CreatureData>(Player.characterData.creatureId);
                 this.BackupfastSwing(cd);
             }
         }
         private void EventManager_onCreatureSpawn(Creature creature)
         {
-            if(creature.isPlayer && OptEnabled)
+            if(creature.isPlayer && OptEnabled && (OptEnabledHome || Level.current.data.id != "Home" ))
             {
+                Player.crouchOnJump = false; // ther pot interrupt view if enabled
                 TogglePlayerMove(false);
                 // TODO: keep crouching
                 creature.currentLocomotion.SetPhysicModifier(
@@ -100,7 +107,7 @@ namespace GettingOverIt
         }
         private void EventManager_onLevelUnload(LevelData levelData, EventTime eventTime)
         {
-            if (eventTime == EventTime.OnStart && OptEnabled)
+            if (Player.currentCreature != null && eventTime == EventTime.OnStart && OptEnabled)
             {
                 this.ApplyCreatureDataValues(Player.currentCreature, this.creatureData_backup) ;
             }
@@ -136,5 +143,6 @@ namespace GettingOverIt
         private CreatureData creatureData_backup = new();
         private const float gravityMult = 0.7f;
         private const float massMult = 0.1f;
+        private bool crouchOnJump = false;
     }
 }
